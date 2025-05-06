315dfd9f54c54d459c495c5ee73ab12ad7bb3475
Aayush Thorat <106389101+aayushthorat27@users.noreply.github.com>
2025-05-06 15:25:33 +0530

    Update cors.js

--- backend/config/cors.js
+++ backend/config/cors.js
@@ -3,7 +3,7 @@
 const cors = require('cors');
 
 const corsOptions = {
-  origin: 'http://localhost:5173',  // Replace with your frontend URL
+  origin: 'https://gimex-ortho-mongo-frontend.vercel.app',  // Replace with your frontend URL
   credentials: true,  // Allows cookies with cross-origin requests
 };
 
